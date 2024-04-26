5f601af346a85d337174ec2018d6000fc6ff3331
Abrar Ahmad Shah <Abrar.Shah@webfleet.com>
2024-04-26 10:15:38 +0530

    ✨ First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+const { PORT } = process.env
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/google', (req, res) => {
+    res.send('abrarshah00@gmail.com')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Chai aur code</h2>')
+})
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
